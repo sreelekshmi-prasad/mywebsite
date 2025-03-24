343e27b1e2388fa4a733b0187f0123811ff29880
Sreelekshmi <sreelekshmi21222@gmail.com>
2025-03-24 22:40:52 +0530

    login and sign-up page added

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint #blueprint separates and can have multiple files
+from flask import Blueprint,render_template #blueprint separates and can have multiple files
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return "<p>Sign Up</p>"
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
