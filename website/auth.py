eb598d914b90e7b777382adb3de1cb52893d24de
Sreelekshmi <sreelekshmi21222@gmail.com>
2025-04-13 14:49:36 +0530

    sign_up and login modified

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template #blueprint separates and can have multiple files
+from flask import Blueprint,render_template,request,flash #blueprint separates and can have multiple files
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,24 @@ def login():
 def logout():
     return "<p>Sign Up</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters',category='error')
+        elif len(firstName) < 2:
+            flash('Email must be greater than 1 characters',category='error')
+        elif password1!=password2:
+            flash('Passwords don\'t match',category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.',category='error')
+        else:
+            flash('Account Created!',category='success')
+            #add user to database
+        
     return render_template("sign_up.html")
