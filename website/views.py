0218e6b4ec1e7bbcc2ba2e2fa87edba140b74cc7
Sreelekshmi-prasad <sreelekshmi21222@gmail.com>
2025-04-26 23:21:00 +0530

    Set up login Page; Home page now requires login

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint ,render_template #blueprint separates and can have multiple files
+from flask_login import login_required,current_user
 
 views = Blueprint('views',__name__)
 
 
 @views.route('/')
+@login_required
 def home():
     return render_template("home.html")
