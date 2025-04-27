e0ae1735dc279b21c9e9f7e4c54929c79b3cdc94
Sreelekshmi-prasad <sreelekshmi21222@gmail.com>
2025-04-27 13:08:36 +0530

    Home and Logout bar changed so that it can be seen only after Login

--- website/views.py
+++ website/views.py
@@ -1,10 +1,26 @@
-from flask import Blueprint ,render_template #blueprint separates and can have multiple files
+from flask import Blueprint ,render_template,flash,request #blueprint separates and can have multiple files
 from flask_login import login_required,current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views',__name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if not note or len(note.strip()) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            try:
+                new_note = Note(data=note, user_id=current_user.id)
+                db.session.add(new_note)
+                db.session.commit()
+                flash('Note Added!', category='success')
+            except Exception as e:
+                db.session.rollback()
+                flash(f"An error occurred while adding the note: {str(e)}", category='error')
+    return render_template("home.html", user=current_user)
