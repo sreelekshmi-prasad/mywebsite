0c786046551fd357021c3fd4f8c8d8a1a9081371
Sreelekshmi-prasad <sreelekshmi21222@gmail.com>
2025-04-27 14:09:21 +0530

    feat: allow adding notes from home page and add delete icon

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint ,render_template,flash,request #blueprint separates and can have multiple files
+from flask import Blueprint ,render_template,flash,request,jsonify #blueprint separates and can have multiple files
 from flask_login import login_required,current_user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint('views',__name__)
 
@@ -12,15 +13,23 @@ def home():
     if request.method == 'POST':
         note = request.form.get('note')
 
-        if not note or len(note.strip()) < 1:
+        if len(note) < 1:
             flash('Note is too short!', category='error')
         else:
-            try:
-                new_note = Note(data=note, user_id=current_user.id)
-                db.session.add(new_note)
-                db.session.commit()
-                flash('Note Added!', category='success')
-            except Exception as e:
-                db.session.rollback()
-                flash(f"An error occurred while adding the note: {str(e)}", category='error')
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note Added!', category='success')
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
